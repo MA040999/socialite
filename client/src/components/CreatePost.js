ecadd221961de37d9d7d7d65220994a9558b3a8a
Muhammed Ahmed <ahmedkhatri1998@gmail.com>
2021-08-28 22:35:12 +0500

    Create post UI improvement

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -1,55 +1,58 @@
-import React, { useState } from 'react'
-import FileBase64 from 'react-file-base64'
-import TextareaAutosize from 'react-textarea-autosize';
+import React, { useState } from "react";
+import FileBase64 from "react-file-base64";
+import TextareaAutosize from "react-textarea-autosize";
+import { RiSendPlaneFill } from "react-icons/ri";
 
 function CreatePost() {
-    const [images, setImages] = useState([])
+  const [images, setImages] = useState([]);
 
-    const removeImage = () => {
-        setImages()
-    }
-   
-    return (
-        <div className="post-container" style={{marginBottom: '70px'}}>
-            <div className="post-heading-container">
-                <div className="post-image">
-                    <img src="/favicon.ico" alt="user" />
-                </div>
-                <TextareaAutosize
-                    name="createPost"
-                    className="post-input"
-                    placeholder="What's on your mind?"
-                    autoComplete="off"
-                />
-                <div className='image-upload-container'>
-                    <FileBase64
-                        type="file"
-                        multiple={true}
-                        onDone={(base64)=> {
-                            setImages([...images, ...base64])  
-                        } }
-                    />
-                </div>
-            </div>
-            {
-                images.length > 0 && <div className="post-images-container  create-post-images">
-                {
-                    images.map( (image,id) =>  <div key={id} className='image-container'>
-                        <img src={ `${image.base64}`} className='image' alt="user" />
-                        <div className="container cross cancel" onClick={removeImage}>
-                            <div className="line"></div>
-                            <div className="line"></div>
-                            <div className="line"></div>
-                        </div>                    
-                    </div>
-                    )
-                }
-            </div>
-            }
-            
-            {/* <pre>{JSON.stringify(images, null, 2)}</pre> */}
+  const removeImage = () => {
+    setImages();
+  };
+
+  return (
+    <div className="post-container" style={{ marginBottom: "70px" }}>
+      <div className="post-heading-container">
+        <div className="post-image">
+          <img src="/favicon.ico" alt="user" />
         </div>
-    )
+        <TextareaAutosize
+          name="createPost"
+          className="post-input"
+          placeholder="What's on your mind?"
+          autoComplete="off"
+        />
+        <div>
+          <RiSendPlaneFill size="2.5rem" />
+          <div className="image-upload-container">
+            <FileBase64
+              type="file"
+              multiple={true}
+              onDone={(base64) => {
+                setImages([...images, ...base64]);
+              }}
+            />
+          </div>
+        </div>
+      </div>
+      {images.length > 0 && (
+        <div className="post-images-container  create-post-images">
+          {images.map((image, id) => (
+            <div key={id} className="image-container">
+              <img src={`${image.base64}`} className="image" alt="user" />
+              <div className="container cross cancel" onClick={removeImage}>
+                <div className="line"></div>
+                <div className="line"></div>
+                <div className="line"></div>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+
+      {/* <pre>{JSON.stringify(images, null, 2)}</pre> */}
+    </div>
+  );
 }
 
-export default CreatePost
+export default CreatePost;
