cc0f991356b80911dd78a2f1ae02433121b1f53c
Muhammed Ahmed <ahmedkhatri1998@gmail.com>
2021-09-18 16:28:18 +0500

    edit profile comments done

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function Footer() {
+  const isComment = useSelector((state) => state.posts.isCommentActive);
+
   return (
-    <div className="footer">
+    <div className={`footer ${isComment ? "footer-comment" : ""}`}>
       &copy; 2021 Made by <b>Muhammed Ahmed</b>
     </div>
   );
